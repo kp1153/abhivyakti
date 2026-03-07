17447af3e86a44c1a3e70dc9535bc5975a78b8e3
kp1153 <kamta_tiwari@yahoo.com>
2026-03-07 14:42:32 +0530

    update epub

--- components/EpubReader.js
+++ components/EpubReader.js
@@ -1,4 +1,4 @@
-"use client"
+ "use client"
 
 import { useState } from "react"
 import { ReactReader } from "react-reader"
@@ -9,7 +9,7 @@ export default function EpubReader() {
   return (
     <div style={{ height: "100vh", userSelect: "none" }}>
       <ReactReader
-        url="https://cdn.sanity.io/files/sqcig83p/production/24c4598585ed377da9fbe23653a85247a021c047.epub"
+        url="https://cdn.sanity.io/files/sqcig83p/production/06d43efb1d70e8691a3bf16d2d7d612304f07125.epub"
         location={location}
         locationChanged={(epubcfi) => setLocation(epubcfi)}
         epubOptions={{ allowScriptedContent: true }}
